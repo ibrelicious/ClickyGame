d326558b703e24b4d8709817352604958b4b928d
ibrelicious <ibrelicious@gmail.com>
2026-04-15 13:48:42 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Click a button or object to earn points so that I can increase my score.
 // See my current score during the game so that I know how well I am doing.
-// See a countdown timer so that I know how much time is left.
+// See a countdown timer so that I know how much time is left. setInterval();
 
 // Variables
 let score = 0;
