f975480b665fa9e4fa3f1137bcccc78357f07dd3
ibrelicious <ibrelicious@gmail.com>
2026-04-20 14:12:46 +0200

    I can submit score now

--- js/app.js
+++ js/app.js
@@ -4,15 +4,37 @@
 
 // Variables
 let score = 0;
+let timeLeft = 5;
+let gameStarted = false;
+let gameEnded = false;
+let interval = null;
 
 // HTML DOM
 const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
 const scoreDisplay = document.getElementById('scoreDisplay');
+const timerDisplay = document.getElementById('timerDisplay');
+const label1 = document.getElementById('label1');
+const input1 = document.getElementById('name');
 
 // UI Functions
 button1.addEventListener('click', () => {
-increaseScore();
+  if (!gameEnded) {
+    increaseScore();
+  }
+
+  if (!gameStarted) {
+    startGame();
+  }
 })
+
+input1.style.display = 'none';
+label1.style.display = 'none';
+
+button2.addEventListener('click', () => {
+  submitHighScore();
+})
+
 
 // Functions
 function increaseScore() {
@@ -20,3 +42,45 @@ function increaseScore() {
   console.log(score);
   scoreDisplay.innerText = score;
 }
+
+function countdown() {
+  timeLeft--;
+  timerDisplay.innerText = timeLeft;
+
+  if (timeLeft <= 0) {
+    timerDisplay.innerText = 0;
+    endGame();
+  }
+}
+
+function startGame() {
+  interval = setInterval(countdown, 1000);
+  gameStarted = true;
+}
+
+function endGame() {
+  gameEnded = true;
+  clearInterval(interval);
+  button1.style.display = 'none';
+  input1.style.display = 'block';
+  label1.style.display = 'block';
+  button2.style.display = 'block';
+}
+
+async function submitHighScore() {
+  console.log(input1.value);
+
+
+  const response = await fetch("https://hooks.zapier.com/hooks/catch/8338993/ujs9jj9/", {
+    method: "POST",
+    body: JSON.stringify({name: input1.value, score: score}),
+  });
+
+  console.log(response);
+}
+
+
+
+
+
+
